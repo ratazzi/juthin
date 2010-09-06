da360dd495d7c36c6986372eb6ec355d59d0bb83
ratazzi <ratazzi.potts@gmail.com>
2010-09-06 19:07:47 +0800

    fixed a bug

--- init.py
+++ init.py
@@ -11,16 +11,18 @@ from juthin.core import Author
 
 class InitHandler(tornado.web.RequestHandler):
     def get(self):
-        author = Author()
-        author.name = u'username'
-        author.nickname = u'nickname'
-        author.passwd = hashlib.md5('password').hexdigest()
-        author.blog_title = u'blog title'
-        author.blog_theme = u'default'
-        author.blog_domain = u'blog domain'
-        author.blog_timezone = 8
-        author.sync_key = hashlib.md5('sync_key').hexdigest() # 通过 web service api 同步文章
-        author.put()
+        author = Author.all().get()
+        if not author:
+            author = Author()
+            author.name = u'username'
+            author.nickname = u'nickname'
+            author.passwd = hashlib.md5('password').hexdigest()
+            author.blog_title = u'blog title'
+            author.blog_theme = u'default'
+            author.blog_domain = u'blog domain'
+            author.blog_timezone = 8
+            author.sync_key = hashlib.md5('sync_key').hexdigest() # 通过 web service api 同步文章
+            author.put()
         self.redirect('/writer/signin/')
 
 class Application(tornado.wsgi.WSGIApplication):
